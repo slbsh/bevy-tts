800c9ef2cbff82804d1df2b29b0adb1ebdf24f05
slbsh <crafter.antek@gmail.com>
2025-03-08 02:17:12 +0100

    wip: egui ui

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use bevy::log::LogPlugin;
 use bevy::prelude::*;
-use bevy_rapier3d::prelude::*;
+#[cfg(feature = "dev")]
 use bevy_inspector_egui::quick::{ResourceInspectorPlugin, WorldInspectorPlugin};
+use bevy_rapier3d::prelude::*;
 
 pub mod camera;
 pub mod game;
@@ -12,16 +13,21 @@ fn main() {
 		.add_plugins((
 			// Cabbage man needs this LogPlugin to filter gpu errors.
 			DefaultPlugins.set(LogPlugin {
-				filter: "wgpu_hal=off".into(),
-				level: bevy::log::Level::INFO,
+				filter:       "wgpu_hal=off".into(),
+				level:        bevy::log::Level::INFO,
 				custom_layer: |_app| None,
 			}),
 			MeshPickingPlugin,
 			RapierPickingPlugin,
 			RapierPhysicsPlugin::<NoUserData>::default().in_schedule(FixedPostUpdate),
 			RapierDebugRenderPlugin::default(),
+			#[cfg(not(feature = "dev"))]
+			bevy_egui::EguiPlugin,
+			#[cfg(feature = "dev")]
 			WorldInspectorPlugin::default(),
+			#[cfg(feature = "dev")]
 			ResourceInspectorPlugin::<MeshPickingSettings>::default(),
+			#[cfg(feature = "dev")]
 			ResourceInspectorPlugin::<user_input::InputState>::default(),
 			user_input::plugin,
 			camera::plugin,
@@ -29,7 +35,7 @@ fn main() {
 		))
 		.insert_resource(ClearColor(Color::srgb(0.97, 0.97, 1.0)))
 		.insert_resource(MeshPickingSettings {
-			require_markers: true,
+			require_markers:     true,
 			ray_cast_visibility: RayCastVisibility::Visible,
 		})
 		.run();
